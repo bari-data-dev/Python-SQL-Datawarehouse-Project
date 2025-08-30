2556a1086638349935fe210bca4dca280af7994e
Bari-asbr <48470795+bari-data-dev@users.noreply.github.com>
2025-08-30 15:17:39 +0700

    Update Integration_fact_sales.sql

--- sql/tools/Integrations/client1/Integration_fact_sales.sql
+++ sql/tools/Integrations/client1/Integration_fact_sales.sql
@@ -1,7 +1,7 @@
 SELECT
   sd.sls_ord_num AS order_number,
-  pr.product_number,
-  cs.customer_id,
+  cs.customer_key,
+  pr.product_key,
   sd.sls_order_dt AS order_date,
   sd.sls_ship_dt AS shipping_date,
   sd.sls_due_dt AS due_date,
@@ -10,5 +10,9 @@ SELECT
   sd.sls_price AS price sd.dwh_batch_id
 FROM
   silver_client1.crm_sales_details sd
-  LEFT JOIN gold_client1.dim_products pr ON sd.sls_prd_key = pr.product_number
-  LEFT JOIN gold_client1.dim_customers cs ON sd.sls_cust_id = cs.customer_id
+  LEFT JOIN gold_client1.dim_products pr 
+      ON sd.sls_prd_key = pr.product_number
+      AND sd.dwh_batch_id = pr.dwh_batch_id
+  LEFT JOIN gold_client1.dim_customers cs 
+      ON sd.sls_cust_id = cs.customer_id
+      AND sd.dwh_batch_id = cs.dwh_batch_id
